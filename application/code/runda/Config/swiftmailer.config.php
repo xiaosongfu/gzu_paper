192bb4bb8484bd0aa2a608d71ab0715ade49989d
xiaosongfu <xiaosong.fu@outlook.com>
2017-05-12 21:19:58 +0800

    Update swiftmailer.config.php

--- application/code/runda/Config/swiftmailer.config.php
+++ application/code/runda/Config/swiftmailer.config.php
@@ -2,6 +2,6 @@
 
 //发送邮件配置
 define("Transport","smtp.sina.com");
-define("UserUame","xiaopxp@sina.com");
-define("Password","GZ*xiaosong79");
+define("UserUame","xia0@sina.com");
+define("Password","xiao");
 define("Name","runda.com");
